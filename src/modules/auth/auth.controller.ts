47967f1b985661bf619be63cf9db831f8ff348b8
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-08-18 16:57:55 +0500

    delete dummy endpoint from auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -13,10 +13,4 @@ export class AuthController {
   async validateUser(@Request() req) {
     return this.authService.login(req.user);
   }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
 }
